77bb9cc71864bf422dd6bbedaa99e08148a1cce7
Jonathan Koss <jonathan_koss@yahoo.com>
2019-12-18 00:04:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,4 +4,8 @@ def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil 
     false
   end 
+  
+  if board[index] == "X" || board[index] == "O"
+    
+  end 
 end  
